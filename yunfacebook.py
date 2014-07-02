44e904e9c832ecb5406ff870739744c6bdea3ce7
sergioesposito <sergio.esposito.perez@alumnos.upm.es>
2014-07-02 17:50:04 +0200

    Update yunfacebook.py

--- yunfacebook.py
+++ yunfacebook.py
@@ -2,5 +2,5 @@
 import facebook
 import sys
 messagefacebook=sys.argv[1]
-graph = facebook.GraphAPI('CAAURY73RwKcBAHrZAHBWZB8ADWWXfXmZBdR9xKZC4k1ZBK5xGZCWybc1FuLlALibJDAnv68efT0QP9h77REpCerNNjMNpKMIHWbRzQNBihU036iBdLqvgpBXbiE37TTRZA3ZC8y05bdqzrwu8q3sPgSkpJvIZBpaQ4yrrlcAuTrXnfGYlL9bhdxBoBMhnw7O4zC8ZD')
+graph = facebook.GraphAPI('xxx')
 graph.put_object("me", "feed", message=messagefacebook)
