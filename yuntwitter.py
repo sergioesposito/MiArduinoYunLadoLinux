02b19ca656e24f47bd7c91ff943be17800ef1e0a
sergioesposito <sergio.esposito.perez@alumnos.upm.es>
2014-07-02 17:50:44 +0200

    Update yuntwitter.py

--- yuntwitter.py
+++ yuntwitter.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 from twython import Twython
 import sys
-OAUTH_TOKEN='78294886-6yO08xifX5Pg87Ra5GkNbDiu4hHf1qnseA2bHu3s2'
-OAUTH_TOKEN_SECRET='2OkHVL4ngFCETEaB32upjJm2FmSjYUqfpYCdcD9mTdwDM'
+OAUTH_TOKEN='xxx'
+OAUTH_TOKEN_SECRET='xxx'
 messagetotwitter=sys.argv[1]
-APP_KEY='ytDXIzPZacr4PS8lbAbBqHcBt'
-APP_SECRET='6lCrwnJMqNz39dFcP9VRdfujsrpZKiZhRV82Zm39Rh943NQGub'
+APP_KEY='xxx'
+APP_SECRET='xxx'
 twitter=Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 twitter.update_status(status=messagetotwitter)
